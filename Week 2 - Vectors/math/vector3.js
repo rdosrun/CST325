6537b1a03fa13ad067c198834f9456ad73578d64
richard <rdosrun@gmail.com>
2021-09-06 17:32:59 -0700

    Add files via upload

---
+++ Week 2 - Vectors/math/vector3.js
@@ -0,0 +1,158 @@
+/*
+ * An "object" representing a 3d vector to make operations simple and concise.
+ *
+ * Similar to how we work with plain numbers, we will work with vectors as
+ * an entity unto itself.  Note the syntax below: var Vector3 = function...
+ * This is different than you might be used to in most programming languages.
+ * Here, the function is meant to be instantiated rather than called and the
+ * instantiation process IS similar to other object oriented languages => new Vector3()
+ */
+
+var Vector3 = function(x, y, z) {
+  this.x = x; this.y = y; this.z = z;
+  if(!this.x && !this.y && !this.z){
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+  }
+  // Sanity check to prevent accidentally using this as a normal function call
+  if (!(this instanceof Vector3)) {
+    console.error("Vector3 constructor must be called with the 'new' operator");
+  }
+
+  // todo - make sure to set a default value in case x, y, or z is not passed in
+}
+
+Vector3.prototype = {
+
+  //----------------------------------------------------------------------------- 
+  set: function(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z =z;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  clone: function() {
+    return new Vector3(this.x, this.y, this.z);
+  },
+
+  //----------------------------------------------------------------------------- 
+  copy: function(other) {
+    this.x = other.x;
+    this.y = other.y;
+    this.z = other.z;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  negate: function() {
+    // multiply 'this' vector by -1
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x *-1;
+    this.y = this.y *-1;
+    this.z = this.z *-1;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  add: function(v) {
+    // todo - add v to 'this' vector
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x +v.x;
+    this.y = this.y+ v.y;
+    this.z = this.z +v.z;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  subtract: function(v) {
+    // todo - subtract v from 'this' vector
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x - v.x;
+    this.y = this.y - v.y;
+    this.z = this.z -v.z;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  multiplyScalar: function(scalar) {
+    // multiply 'this' vector by "scalar"
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x *scalar;
+    this.y = this.y *scalar;
+    this.z = this.z *scalar;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  length: function() {
+    // todo - return the magnitude (A.K.A. length) of 'this' vector
+    // This should NOT change the values of this.x, this.y, and this.z
+    var tmp = (this.x*this.x)+(this.y*this.y)+(this.z*this.z);
+    tmp = Math.sqrt(tmp);
+    return tmp;
+  },
+
+  //----------------------------------------------------------------------------- 
+  lengthSqr: function() {
+    // todo - return the squared magnitude of this vector ||v||^2
+    // This should NOT change the values of this.x, this.y, and this.z
+
+    // There are many occasions where knowing the exact length is unnecessary 
+    // and the square can be substituted instead (for performance reasons).  
+    // This function should not have to take the square root of anything.
+    var tmp = (this.x*this.x)+(this.y*this.y)+(this.z*this.z);
+    return tmp;
+  },
+
+  //----------------------------------------------------------------------------- 
+  normalize: function() {
+    // todo - Change the components of this vector so that its magnitude will equal 1.
+    // This SHOULD change the values of this.x, this.y, and this.z
+    var len = this.length();
+    this.x = this.x/len;
+    this.y = this.y/len;
+    this.z = this.z/len;
+    return this;
+  },
+
+  //----------------------------------------------------------------------------- 
+  dot: function(other) {
+    // todo - return the dot product betweent this vector and "other"
+    // This should NOT change the values of this.x, this.y, and this.z
+    var tmp = (this.x*other.x)+(this.y*other.y)+(this.z*other.z);
+    return tmp;
+  },
+
+
+  //============================================================================= 
+  // The functions below must be completed in order to receive an "A"
+
+  //----------------------------------------------------------------------------- 
+  fromTo: function(fromPoint, toPoint) {
+    if (!(fromPoint instanceof Vector3) || !(toPoint instanceof Vector3)) {
+      console.error("fromTo requires to vectors: 'from' and 'to'");
+    }
+
+    // todo - return the vector that goes from "fromPoint" to "toPoint"
+    //        NOTE - "fromPoint" and "toPoint" should not be altered
+    var tmp = toPoint.clone();
+    tmp.subtract(fromPoint);
+    return(tmp)
+  },
+
+  //----------------------------------------------------------------------------- 
+  project: function(vectorToProject, otherVector) {
+    // todo - return a vector that points in the same direction as "otherVector"
+    //        but whose length is the projection of "vectorToProject" onto "otherVector"
+    //        NOTE - "vectorToProject" and "otherVector" should NOT be altered (i.e. use clone)
+    //        See class slides or visit https://en.wikipedia.org/wiki/Vector_projection for more detail.
+    var tmp = otherVector.clone();
+    tmp.normalize();
+    tmp.multiplyScalar(vectorToProject.length())
+  }
+};
+
+// EOF 00100001-10
